3ba7b4ec7e8a02229f548021422267b86a7e20e9
unknown <deepapaikar19@gmail.com>
2018-10-09 11:10:54 +0530

    Thsi is first commit

---
+++ src/main/java/com/sm/SendEmailEg.java
@@ -0,0 +1,43 @@
+package com.sm;
+
+import java.util.Properties;    
+import javax.mail.*;    
+import javax.mail.internet.*;    
+class Mailer{  
+    public static void send(String from,String password,String to,String sub,String msg){  
+          //Get properties object    
+          Properties props = new Properties();    
+          props.put("mail.smtp.host", "smtp.gmail.com");    
+          props.put("mail.smtp.socketFactory.port", "465");    
+          props.put("mail.smtp.socketFactory.class",    
+                    "javax.net.ssl.SSLSocketFactory");    
+          props.put("mail.smtp.auth", "true");    
+          props.put("mail.smtp.port", "465");    
+          //get Session   
+          Session session = Session.getDefaultInstance(props,    
+           new javax.mail.Authenticator() {    
+           protected PasswordAuthentication getPasswordAuthentication() {    
+           return new PasswordAuthentication(from,password);  
+           }    
+          });    
+          //compose message    
+          try {    
+           MimeMessage message = new MimeMessage(session);    
+           message.addRecipient(Message.RecipientType.TO,new InternetAddress(to));  
+           message.setFrom(new InternetAddress("vskreddy652@yahoo.com"));
+           message.setSubject(sub);    
+           message.setText(msg);    
+           //send message  
+           Transport.send(message);    
+           System.out.println("message sent successfully");    
+          } catch (MessagingException e) {throw new RuntimeException(e);}    
+             
+    }  
+}  
+public class SendEmail{    
+ public static void main(String[] args) {    
+     //from,password,to,subject,message  
+     Mailer.send("vskreddy6814@gmail.com","Password","vskreddy652@yahoo.com","hello am doing fine","How r u?");  
+     //change from, password and to  
+ }    
+}    
